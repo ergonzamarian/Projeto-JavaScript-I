9337aa99676e1527afa6c1504bb61f5ad12a81c3
ergonzamarian <ergonzamarian@gmail.com>
2022-08-02 18:02:05 -0400

    teste commit

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -9,7 +9,6 @@ while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     //template String
     const idAudio = `#som_${instrumento}`;
 
