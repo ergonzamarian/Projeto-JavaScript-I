4b8e2fea6cef948937b966b4380064159bd50da9
ergonzamarian <ergonzamarian@gmail.com>
2022-08-02 19:16:03 -0400

    Adicionando boas práticas em meu arquivo main.js e finalizado projeto

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,11 +1,17 @@
 function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+        
+    }else{
+        alert('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -15,5 +21,13 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function(){
         tocaSom(idAudio)
     };
-    contador++;
+    tecla.onkeydown = function(evento) {
+        
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
+    };
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    };
 }
